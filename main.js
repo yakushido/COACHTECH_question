a6689efacc884a401f06a5e99fc6289d61db7c55
yuki <y.yaku.0131@gmail.com>
2022-02-21 11:37:19 +0900

    疑似要素deno

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
+// ハンバーガーメニュー
 const target = document.getElementById("hamburger-menu");
 
 target.addEventListener('click', () => {
   target.classList.toggle('active');
 });
 
-const target2 = document.getElementById("hamburger-menu2--bar");
+const target2 = document.getElementById("hamburger-menu2");
+const barToggle = document.getElementById("hamburger-menu2--bar");
+const barMdlToggle = document.getElementById(".hamburger-menu2--bar--middle");
 
 target2.addEventListener('click', () => {
-  target2.classList.toggle('active');
+  barToggle.classList.toggle('active');
+  barMdlToggle.classList.toggle('active');
 });
+
+// ドロワーメニュー
